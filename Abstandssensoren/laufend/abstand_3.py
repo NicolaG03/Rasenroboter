d22d206e4f117146e09ed97f4f3c889087df7c95
Marco-2021 <80843570+Marco-2021@users.noreply.github.com>
2021-09-02 19:10:30 +0200

    Lauffähige Programme
    schlecht gelöst dafür funktionierend

---
+++ Abstandssensoren/laufend/abstand_3.py
@@ -0,0 +1,12 @@
+import abstand_0 as a
+import abstand_1 as b
+import abstand_2 as c
+import time
+
+while True:
+	a.auslesen()
+	time.sleep(1)
+	b.auslesen()
+	time.sleep(1)
+	c.auslesen()
+	time.sleep(1)
